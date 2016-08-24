9947afff14523bba065207a83da5f70e5f32ddfb
dgriego <daniel@jackrabbitsystems.com>
2016-08-23 18:09:14 -0600

    adds validations

--- todo.rb
+++ todo.rb
@@ -1,7 +1,6 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
-require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
 # setting up a configuration
 # that enables sintras session capabilities
@@ -14,17 +13,38 @@ before do
   session[:lists] ||= []
 end
 
-get "/" do
-  redirect "/lists"
+get '/' do
+  redirect '/lists'
 end
 
-get "/lists" do
+get '/lists' do
   @lists = session[:lists]
 
   erb :lists
 end
 
-get "/lists/new" do
-  session[:lists] << { name: "new list", todos: [] }
-  redirect "/lists"
+get '/lists/new' do
+  erb :new_list
+end
+
+def error_for_list_name(name)
+  if !(1..100).cover? name.size
+    'List name must be between 1 and 100 characters.'
+  elsif session[:lists].any? { |list| list[:name] == name }
+    'List name must be unique.'
+  end
+end
+
+post '/lists' do
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  end
 end
